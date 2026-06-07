1d5e636d829ac08d8d54260282deb499f3004cd3
mdhesi <mdhesi334@gmail.com>
2026-06-06 20:16:57 -0700

    Initial Commit

---
+++ BrewGUI/BrewGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewGUIApp.swift
+//  BrewGUI
+//
+//  Created by Manny Dhesi on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct BrewGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
